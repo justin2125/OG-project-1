43f52813ce2f5193c9ad7619eb692cd149dc8ab7
Justin <Justinlall@icloud.com>
2022-10-03 11:16:04 -0400

    starting functions

--- game.js
+++ game.js
@@ -1,4 +1,11 @@
-const frontA = document.getElementsByClassName('frontA')
+const frontA = document
+  .getElementsByClassName('frontA')
+  .addEventListener('click', flipCard)
 const frontb = document.getElementsByClassName('frontB')
-const backA = document.getElementsByClassName('backA')
+const backA = document
+  .getElementsByClassName('backA')
+  .addEventListener('click', flipCard)
 const backB = document.getElementsByClassName('backB')
+const all = document.querySelectorAll('div')
+
+let start = (startgame) => {}
