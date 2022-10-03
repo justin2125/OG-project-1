2b8fd4b76f942b2a2808c1dd7c23f5e6c6065280
Justin <Justinlall@icloud.com>
2022-10-03 10:49:27 -0400

    listed variables and divs

---
+++ game.js
@@ -0,0 +1,4 @@
+const frontA = document.getElementsByClassName('frontA')
+const frontb = document.getElementsByClassName('frontB')
+const backA = document.getElementsByClassName('backA')
+const backB = document.getElementsByClassName('backB')
