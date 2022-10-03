0f5cbfa273f3f4e1302dac640dd1267279f26a60
Justin <Justinlall@icloud.com>
2022-10-03 11:41:10 -0400

    finished toggle functions

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 const frontA = document
   .getElementsByClassName('frontA')
-  .addEventListener('click', flipCard)
+  .addEventListener('click', clickFlipA)
 const frontb = document.getElementsByClassName('frontB')
 const backA = document
   .getElementsByClassName('backA')
   .addEventListener('click', flipCard)
 const backB = document.getElementsByClassName('backB')
 const all = document.querySelectorAll('div')
+
+let flipA = (flip) => {
+  frontA.classList.toggle(backA)
+}
+
+let flipB = (flip) => {
+  backA.classList.toggle(backB)
+}
 
 let start = (startgame) => {}
