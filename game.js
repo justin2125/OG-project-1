0c849c9afbc68ac82d71dcd4c3a9f3a88d195f80
Justin <Justinlall@icloud.com>
2022-10-03 19:21:33 -0400

    added flip function onto both cards

--- game.js
+++ game.js
@@ -1,21 +1,44 @@
-const frontA = document
-  .getElementsByClassName('.frontA')
-  .addEventListener('click', flipA())
-const frontb = document.getElementsByClassName('.frontB')
-const backA = document
-  .getElementsByClassName('.backA')
-  .addEventListener('click', flipB())
-const backB = document.getElementsByClassName('.backB')
-const all = document.querySelectorAll('div')
+const frontA = 1
+const frontB = 2
+const backA = 3
+const backB = 3
+document.getElementByClassName('frontA').addEventListener('click', flipA)
 
-let flipA = (flip) => {
-  frontA.classList.toggle(backA)
+const card = document.getElementById('card')
+
+card.addEventListener('click', flipCard)
+
+flipCard = () => {
+  card.classList.toggle('flipCard')
 }
+const card2 = document.getElementById('card2')
+card2.addEventListener('click', flipCard2)
 
-let flipB = (flip) => {
+document.getElementsByClassName('.frontA').addEventListener('click', flipA())
+//
+document.getElementsByClassName('.frontB')
+//
+document.getElementsByClassName('.backA').addEventListener('click', flipB())
+//
+document.getElementsByClassName('.backB')
+//
+document.querySelectorAll('div')
+
+frontB.addEventListener('click', flipB())
+let flipB = (flips) => {
   backA.classList.toggle(backB)
 }
 
-let start = (game) => {}
-
-let timer = (time) => {}
+let start = (game) => {
+  alert('The Game has begun !')
+  console.log('testing testing')
+  if (backA === backB) {
+    console.log('that is a match, great job!!')
+  }
+}
+let timer = (time) => {
+  alert('TIMES UPPPP')
+  setTimeout(timer(), 90000)
+}
+setTimeout(timer(), 90000)
+//start()
