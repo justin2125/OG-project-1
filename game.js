57c917c95df6466a097edf7a69512587ba15ef15
Justin <Justinlall@icloud.com>
2022-10-03 12:17:41 -0400

    updated CSS slightly

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 const frontA = document
-  .getElementsByClassName('frontA')
-  .addEventListener('click', clickFlipA)
-const frontb = document.getElementsByClassName('frontB')
+  .getElementsByClassName('.frontA')
+  .addEventListener('click', flipA())
+const frontb = document.getElementsByClassName('.frontB')
 const backA = document
-  .getElementsByClassName('backA')
-  .addEventListener('click', flipCard)
-const backB = document.getElementsByClassName('backB')
+  .getElementsByClassName('.backA')
+  .addEventListener('click', flipB())
+const backB = document.getElementsByClassName('.backB')
 const all = document.querySelectorAll('div')
 
 let flipA = (flip) => {
@@ -16,4 +16,6 @@ let flipB = (flip) => {
   backA.classList.toggle(backB)
 }
 
-let start = (startgame) => {}
+let start = (game) => {}
+
+let timer = (time) => {}
